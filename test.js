880c705b55eb3bda628a8bce27baeda5ca5a5305
Lionel T <elrumordelaluz@hotmail.com>
2018-06-06 15:24:21 +0200

    Update test

--- test.js
+++ test.js
@@ -12,7 +12,6 @@ test('Scale that SVG!', async t => {
   const scaled = await scaleThatSvg(input, { scale })
   const _input = await svgson(input.toString())
   const _scaled = await svgson(scaled)
-
   t.is(
     Number(_scaled.attributes.viewBox.split(' ')[2]),
     _input.attributes.viewBox.split(' ')[2] * scale
@@ -21,5 +20,5 @@ test('Scale that SVG!', async t => {
     Number(_scaled.attributes.viewBox.split(' ')[3]),
     _input.attributes.viewBox.split(' ')[3] * scale
   )
-  t.not(scaled, expected.toString())
+  t.is(scaled, expected.toString())
 })
